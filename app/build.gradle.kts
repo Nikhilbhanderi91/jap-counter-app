51efbc640884a20e5b77c7cfb5fcef5f530bb432
Nikhilbhanderi91 <nikhilbhanderi4410@gmail.com>
2026-09-09 20:50:22 +0530

    report added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.japcounter.jap_counter_app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
